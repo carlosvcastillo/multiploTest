8c464c604da1fe8578766117581b217c00e4524d
carlos <carlos@demotest.com>
2024-03-14 23:12:03 -0600

    lastupdates

--- testApp/app/Http/Controllers/MultiploTestController.php
+++ testApp/app/Http/Controllers/MultiploTestController.php
@@ -7,28 +7,31 @@ use Illuminate\Http\Request;
 
 class MultiploTestController extends Controller
 {
-
+    //Aqui genere esta variable para identificar cuales eran los multiplos que solicitaban y agregue el color de cada uno de los mismos para usarlos mas adelante
     private $_multiplos = [
         3 => 'green',
         5 => 'red',
         7 => 'blue'
     ];
-
+    //Esta funcion es la encargada de realizar la operacion y retornar los posibles multiplos encontrados con el numero que el usuario ingreso
     function obtenerMultiplos(Request $request) {
         $allMultiplos=array();
         $multiploResultado = [];
         $numero = $request->input('numero');
 
+        //Se hace la validacion en caso de que el usuario ingrese un numero menor o igual a cero y retorna los posibles resultados a mi vista
         if($numero <= 0) {
             return view('test', ['result' => $multiploResultado, 'usuarioNumero' => $numero, 'color' => $this->_multiplos]);
         }
 
+        //Recorro mi variable estatica con los numeros solicitados para su validacion de multiplos y en caso de encontrar los almacena en un array
         foreach ($this->_multiplos as $key => $value) {
             if ($numero%$key == 0) {
                 $multiploResultado[] = $key; 
             }
         }
 
+        //Aqui almaceno los valores obtenidos en mi tabla
         Results::create(
             [
                 'numeroUsuario' => $numero,
--- testApp/routes/web.php
+++ testApp/routes/web.php
@@ -15,7 +15,10 @@ use App\Http\Controllers\MultiploTestController;
 |
 */
 
+//Esta ruta la ocupe como vista principal para mostrar el formulario
 Route::get('api/test', function () {
     return view('test');
 });
+
+//Esta ruta la ocupe para realizar la operacion logica del ejercicio para el cual estoy usando un alias para ocuparlo en la vista (el alias es 'name')
 Route::post('api/test', [MultiploTestController::class, 'obtenerMultiplos'])->where('numero', '[0-9]+')->name('notes.test');
